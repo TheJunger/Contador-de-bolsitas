d3e5872ea53cb09df5f11f036a1ad8f81d1678ae
= <=>
2024-06-24 18:34:15 -0300

    openlabel handle major changes

--- bolsonfront/src/OpenLabel.js
+++ bolsonfront/src/OpenLabel.js
@@ -3,60 +3,75 @@ import React, { useState, useEffect } from "react";
 const OpenLabel = ({ setShowLabelEdit, bolsitaid, tipoBolsita, fetchData, token }) => {
   const [dataEditBolsitas, setDataEditBolsitas] = useState([]);
   const [selladas, setSelladas] = useState(0);
+  const [sinSellar, setSinSellar] = useState(0);
 
   useEffect(() => {
     fetch(`https://thejunger.pythonanywhere.com/api/get-specific-data`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-         "Authorization": `Bearer ${token}`,
+        "Authorization": `Bearer ${token}`,
       },
       body: JSON.stringify({ bolsitaid: bolsitaid })
     })
-    .then(response => response.json())
-    .then(data => {
-      setDataEditBolsitas(data);
-      if (data.length > 0 && tipoBolsita == 'selladas') {
-        setSelladas(data[0].Selladas); // Inicializar el valor del input
-      }
-      else if (data.length > 0 && tipoBolsita == 'Sin Sellar') {
-        setSelladas(data[0].Sin_Sellar); // Inicializar el valor del input
-      }
-    });
+      .then(response => response.json())
+      .then(data => {
+        setDataEditBolsitas(data);
+        if (data.length > 0) {
+          setSelladas(data[0].Selladas);
+          setSinSellar(data[0].Sin_Sellar);
+        }
+      });
   }, [bolsitaid]);
 
   const handleSave = async (e) => {
     e.preventDefault();
+
     const response = await fetch('https://thejunger.pythonanywhere.com/api/save-bolsita', {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
         "Authorization": `Bearer ${token}`,
       },
-      body: JSON.stringify({ bolsitaid: bolsitaid, nuevoValor: selladas, tipoACambiar: tipoBolsita, total: (parseInt(selladas) + parseInt(dataEditBolsitas[0].Sin_Sellar)) })
-    })
+      body: JSON.stringify({
+        bolsitaid: bolsitaid,
+        nuevoValorSelladas: selladas,
+        nuevoValorSinSellar: sinSellar,
+      })
+    });
 
-    const data = await response.text()
-    if (response.ok){
-      setShowLabelEdit(false)
+    if (response.ok) {
+      setShowLabelEdit(false);
       fetchData();
+    } else {
+      alert('Ha sucedido un problema durante el guardado');
     }
-    else{
-      alert('Ha sucedido un problema durante el guardado')
-    }
-
-    //.then(() => {
-    //  setShowLabelEdit(false);
-    //  fetchData();  // Llamar a fetchData para actualizar los datos en App
-    //});
   };
 
   const handleChange = (e) => {
-    setSelladas(e.target.value);
+    const newValue = parseInt(e.target.value);
+    if (tipoBolsita === "selladas") {
+      const difference = newValue - selladas;
+      setSelladas(newValue);
+      setSinSellar(sinSellar - difference);
+    } else if (tipoBolsita === "Sin Sellar") {
+      setSinSellar(newValue);
+    }
   };
 
   const handleCancel = () => {
     setShowLabelEdit(false);
+  };
+
+  const adjustValue = (amount) => {
+    if (tipoBolsita === "selladas") {
+      const newSelladas = selladas + amount;
+      const newSinSellar = sinSellar - amount;
+      setSelladas(newSelladas);
+      setSinSellar(newSinSellar);
+    } else if (tipoBolsita === "Sin Sellar") {
+      setSinSellar(sinSellar + amount);
+    }
   };
 
   return (
@@ -71,29 +86,29 @@ const OpenLabel = ({ setShowLabelEdit, bolsitaid, tipoBolsita, fetchData, token 
           'Cargando'
         )}
         <div className="helplabel">
-          <div>S : {dataEditBolsitas.length > 0 ? (dataEditBolsitas[0].Selladas): ('cargando')}</div>
-          <div>SS: {dataEditBolsitas.length > 0 ? (dataEditBolsitas[0].Sin_Sellar): ('cargando')}</div>
+          <div>S : {dataEditBolsitas.length > 0 ? dataEditBolsitas[0].Selladas : 'cargando'}</div>
+          <div>SS: {dataEditBolsitas.length > 0 ? dataEditBolsitas[0].Sin_Sellar : 'cargando'}</div>
         </div>
         <div className="buttonsnumerlabel">
-          <div onClick={() => setSelladas(selladas - 1200)}>-1200</div>
-          <div className="buttonnumerhidden" onClick={() => setSelladas(selladas - 1100)}>-1100</div>
-          <div className="buttonnumerhiddendos" onClick={() => setSelladas(selladas - 1000)}>-1000</div>
-          <div className="buttonnumerhiddenphone" onClick={() => setSelladas(selladas - 900)}>-900</div>
-          <div className="buttonnumerhiddenphone" onClick={() => setSelladas(selladas - 600)}>-600</div>
-          <div className="buttonnumerhiddenphone" onClick={() => setSelladas(selladas - 500)}>-500</div>
+          <div onClick={() => adjustValue(-1200)}>-1200</div>
+          <div className="buttonnumerhidden" onClick={() => adjustValue(-1100)}>-1100</div>
+          <div className="buttonnumerhiddendos" onClick={() => adjustValue(-1000)}>-1000</div>
+          <div className="buttonnumerhiddenphone" onClick={() => adjustValue(-900)}>-900</div>
+          <div className="buttonnumerhiddenphone" onClick={() => adjustValue(-600)}>-600</div>
+          <div className="buttonnumerhiddenphone" onClick={() => adjustValue(-500)}>-500</div>
           <input
             type="number"
             onChange={handleChange}
-            value={selladas}
+            value={tipoBolsita === "selladas" ? selladas : sinSellar}
             className="inputlabelcont"
-            placeholder={selladas}
+            placeholder={tipoBolsita === "selladas" ? selladas : sinSellar}
           />
-          <div className="buttonnumerhiddenphone" onClick={() => setSelladas(selladas + 500)}>+500</div>
-          <div className="buttonnumerhiddenphone" onClick={() => setSelladas(selladas + 600)}>+600</div>
-          <div className="buttonnumerhiddenphone" onClick={() => setSelladas(selladas + 900)}>+900</div>
-          <div className="buttonnumerhiddendos" onClick={() => setSelladas(selladas + 1000)}>+1000</div>
-          <div className="buttonnumerhidden" onClick={() => setSelladas(selladas + 1100)}>+1100</div>
-          <div onClick={() => setSelladas(selladas + 1200)}>+1200</div>
+          <div className="buttonnumerhiddenphone" onClick={() => adjustValue(500)}>+500</div>
+          <div className="buttonnumerhiddenphone" onClick={() => adjustValue(600)}>+600</div>
+          <div className="buttonnumerhiddenphone" onClick={() => adjustValue(900)}>+900</div>
+          <div className="buttonnumerhiddendos" onClick={() => adjustValue(1000)}>+1000</div>
+          <div className="buttonnumerhidden" onClick={() => adjustValue(1100)}>+1100</div>
+          <div onClick={() => adjustValue(1200)}>+1200</div>
         </div>
         <div className="buttonssavelabel">
           <button className="cancelbutonlabel" onClick={handleCancel}>Cancelar</button>
